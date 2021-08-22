dc187e21f94e07b04aa955387a7790941431e39f
Julian Feinauer <j.feinauer@pragmaticminds.de>
2021-08-22 14:55:02 +0200

    Fixed oauth

--- iotdb_cloud/settings.py
+++ iotdb_cloud/settings.py
@@ -30,7 +30,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 SECRET_KEY = 'django-insecure-5psd^e-4e(2tvbdgtzi3z0@^es&6k+9z6_3aw6i=^vr&($n6q-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG", default=True)
 
 ALLOWED_HOSTS = ["*"]
 
@@ -171,10 +171,13 @@ SOCIALACCOUNT_PROVIDERS = {
         # credentials, or list them here:
         'APP': {
             'client_id': env("GITHUB_CLIENT_ID", default=""),
-            'secret': env("GITHUB_CLIENT_ID", default=""),
+            'secret': env("GITHUB_CLIENT_SECRET", default=""),
             'key': env("GITHUB_KEY", default="")
         }
     }
 }
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+ACCOUNT_DEFAULT_HTTP_PROTOCOL='http' if DEBUG else "https"
